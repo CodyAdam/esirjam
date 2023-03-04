139bb42eb5fc28890e79aab5f36f34165efd0446
Pyrai <gabin.chapr@gmail.com>
2023-03-04 03:16:33 +0100

    Add EnemySpawner

--- Assets/Scripts/Ennemy.cs
+++ Assets/Scripts/Ennemy.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class Ennemy : MonoBehaviour
 {
-
+    float speed = 5f;
     Color startColor;
+    Transform target;
+    Vector3 pos;
+
+    public Transform GetTarget()
+    {
+        return target;
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
 
     private void Start()
     {
@@ -25,5 +37,27 @@ public class Ennemy : MonoBehaviour
     private void OnMouseDown()
     {
         GameManager.instance.player.GetComponent<PlayerController>().SetTarget(transform);
+    }
+
+    private void Update()
+    {
+        if (target != null)
+        {
+            //Si il est sur la cible, il la détruit
+            if (Vector3.Distance(target.position, transform.position) <= 0.1)
+            {
+                Destroy(gameObject);
+            }
+
+            //Sinon, il avance vers elle
+            else
+            {
+                Vector3 direction = (target.position - transform.position).normalized;
+                pos = transform.position + direction * speed * Time.deltaTime;
+                pos.y = transform.position.y;
+                transform.position = pos;
+            }
+
+        }
     }
 }
