b5a56664c8fba4dd80cd1d9bf67fa5fbde6d9380
Pyrai <gabin.chapr@gmail.com>
2023-03-04 02:05:20 +0100

    rectifie Move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Vector3 pos;
-    public float speed = 10f;
+    public float speed;
 
 
     void Update()
     {
-        Vector3 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
-        pos = transform.position + direction * speed * Time.deltaTime;
+        Vector3 direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+        pos = transform.position + direction.normalized * speed * Time.deltaTime ;
         pos.y = transform.position.y;
         transform.position = pos;
 
